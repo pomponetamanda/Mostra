e450b024852d5d800f6688f2c931c30e5d644f38
ANAILA MATOS SILVA <021.900239@senaicimatec.edu.br>
2024-10-19 12:16:10 -0300

    api asaas

---
+++ Mostra/Pages/produto2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mostra.Pages
+{
+    public class produto2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
